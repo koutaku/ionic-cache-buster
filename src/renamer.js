154e99770084896470df73dcbd47ef43a736ab70
Josh Beard <Josh.Beard@AcumenMD.com>
2017-03-16 12:10:45 -0400

    Added sourcemapping reference rewrite.

--- src/renamer.js
+++ src/renamer.js
@@ -6,6 +6,7 @@ var nameGenerator = require("./name-generator");
 const fileNotFound = "file or directory not found.";
 
 const defaultIndexFileName = "index.html";
+const defaultMainJsFileName = "main.js";
 
 const wwwPath = "/www";
 const fullWwwPath = path.resolve(path.resolve() + wwwPath);
@@ -19,20 +20,33 @@ const defaultBrowserPath = path.resolve(path.resolve() + browserPath);
 const browserBuildPath = "/platforms/browser" + buildPath;
 const defaultBrowserBuildPath = path.resolve(path.resolve() + browserBuildPath);
 
-function handleRewrite(indexPath, generatedNames) {
-    console.log("Reading " + indexPath);
+function rewriteIndex(indexPath, generatedNames) {
+    console.log("Reading Index From " + indexPath);
     var data = fs.readFileSync(indexPath, { encoding: "utf8" });
-    console.log("Done reading");
+    console.log("Done Reading Index");
 
-    console.log("replacing data");
+    console.log("Replacing Data");
     var result = data.replace(/main.js/g, generatedNames.mainName);
     result = result.replace(/main.js.map/g, generatedNames.mainName + ".map");
     result = result.replace(/main.css/g, generatedNames.styleName);
     result = result.replace(/polyfills.js/g, generatedNames.polyName);
 
-    console.log("Writing " + indexPath);
+    console.log("Writing Index To " + indexPath);
     fs.writeFileSync(indexPath, result, { encoding: "utf8" });
     console.log("Done writing Index");
+};
+
+function rewriteMain(mainPath, generatedNames) {
+    console.log("Reading Main From " + mainPath);
+    var data = fs.readFileSync(mainPath, { encoding: "utf8" });
+    console.log("Done reading Main");
+
+    console.log("Replacing Data");
+    var result = data.replace(/sourceMappingURL=main.js.map/g, "sourceMappingURL=" + generatedNames.mainName + ".map");
+
+    console.log("Writing Main To " + mainPath);
+    fs.writeFileSync(mainPath, result, { encoding: "utf8" });
+    console.log("Done Writing Main");
 };
 
 module.exports = function(fullBuildPath) {
@@ -50,12 +64,12 @@ module.exports = function(fullBuildPath) {
     var generatedNames = nameGenerator();
 
     var renameObjects = [
-        { current: path.resolve(fullBuildPath + "/main.js"), new: path.resolve(fullBuildPath + "/" + generatedNames.mainName) },
-        { current: path.resolve(fullBuildPath + "/main.js.map"), new: path.resolve(fullBuildPath + "/" + generatedNames.mainName + ".map") },
+        { current: path.resolve(fullBuildPath + "/" + defaultMainJsFileName), new: path.resolve(fullBuildPath + "/" + generatedNames.mainName) },
+        { current: path.resolve(fullBuildPath + "/"+ defaultMainJsFileName + ".map"), new: path.resolve(fullBuildPath + "/" + generatedNames.mainName + ".map") },
         { current: path.resolve(fullBuildPath + "/main.css"), new: path.resolve(fullBuildPath + "/" + generatedNames.styleName) },
         { current: path.resolve(fullBuildPath + "/polyfills.js"), new: path.resolve(fullBuildPath + "/" + generatedNames.polyName) },
-        { current: path.resolve(fullBrowserBuildPath + "/main.js"), new: path.resolve(fullBrowserBuildPath + "/" + generatedNames.mainName) },
-        { current: path.resolve(fullBrowserBuildPath + "/main.js.map"), new: path.resolve(fullBrowserBuildPath + "/" + generatedNames.mainName + ".map") },
+        { current: path.resolve(fullBrowserBuildPath + "/" + defaultMainJsFileName), new: path.resolve(fullBrowserBuildPath + "/" + generatedNames.mainName) },
+        { current: path.resolve(fullBrowserBuildPath + "/"+ defaultMainJsFileName + ".map"), new: path.resolve(fullBrowserBuildPath + "/" + generatedNames.mainName + ".map") },
         { current: path.resolve(fullBrowserBuildPath + "/main.css"), new: path.resolve(fullBrowserBuildPath + "/" + generatedNames.styleName) },
         { current: path.resolve(fullBrowserBuildPath + "/polyfills.js"), new: path.resolve(fullBrowserBuildPath + "/" + generatedNames.polyName) }
     ];
@@ -72,17 +86,27 @@ module.exports = function(fullBuildPath) {
         }
     });
 
-    console.log("Getting Index");
+    console.log("Getting Index and Main");
 
     var indexFilePath = path.resolve(fullWwwPath + "/" + defaultIndexFileName);
+    var mainJsPath = path.resolve(fullBuildPath + "/" + defaultMainJsFileName);
 
     if(fs.existsSync(indexFilePath)) {
-        handleRewrite(indexFilePath, generatedNames);
+        rewriteIndex(indexFilePath, generatedNames);
+    }
+
+    if(fs.existsSync(mainJsPath)) {
+        rewriteMain(mainJsPath, generatedNames);
     }
 
     var browserIndexFilePath = path.resolve(defaultBrowserPath + "/" + defaultIndexFileName);
+    var browserMainJsPath = path.resolve(defaultBrowserPath + "/" + defaultMainJsFileName);
 
     if(fs.existsSync(browserIndexFilePath)) {
-        handleRewrite(browserIndexFilePath, generatedNames);
+        rewriteIndex(browserIndexFilePath, generatedNames);
+    }
+
+    if(fs.existsSync(browserMainJsPath)) {
+        rewriteMain(browserMainJsPath, generatedNames);
     }
 }
